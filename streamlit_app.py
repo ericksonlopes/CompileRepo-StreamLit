3069b743ef88e0ce994a93d304a9d7e83605f7ab
Erickson Lopes <ofc.erickson@gmail.com>
2023-04-21 15:32:59 -0300

    up project

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,7 @@
-import findspark
+import pandas as pd
 import streamlit as st
 
 from helpers import GitHub
-
-findspark.init()
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("Gerador de árvore de diretórios").getOrCreate()
 
 st.title("Gerador de árvore de diretórios")
 
@@ -20,11 +15,9 @@ github = GitHub()
 github.generate_tree(repositorio)
 
 st.text("Arquivos: ")
-rdd_files = spark.sparkContext.parallelize(github.files)
-df_files = rdd_files.toDF()
-st.dataframe(df_files)
+df_file = pd.DataFrame(github.files)
+st.table(df_file)
 
 st.text("Diretórios: ")
-rdd_directory = spark.sparkContext.parallelize(github.directories)
-df_directory = rdd_directory.toDF()
-st.dataframe(df_directory)
+df_dir = pd.DataFrame(github.directories)
+st.table(df_dir)
